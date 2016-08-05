0a35d5fffed9a82d7579b03711f5b9f82567ef8b
Adam Back <adb33@email.arizona.edu>
2016-08-05 11:22:46 -0700

    (fix) Was misusing next, resulting in 404.

--- routes/index.js
+++ routes/index.js
@@ -20,22 +20,41 @@ router.get('/airlines', function( req, res ) {
 
 router.get('/airports', function( req, res, next ) {
   // Lists all matching airports from the Flight API.
-
-  if ( Object.keys( req.query ).length ) {
-    next( 'route' );
+  if ( req.query.q ) {
+    next();
   } else {
     var err = new Error( 'Please provide a city to search by as a querystring.' );
     err.status = 409;
     next( err );
   }
- }, function( req, res ) {
-  thirdPartyHelpers.getAirports( req.originalUrl )
+ }, function( req, res, next ) {
+  thirdPartyHelpers.getAirports( req )
   .then(function( airports ) {
     res.send( airports );
   })
   .catch(function( error ) {
     next( error );
   });
- });
+});
+
+router.get('/search', function( req, res, next ) {
+  // Performs a flight search with to, from, and date
+
+  if ( req.query.to && req.query.from && req.query.date ) {
+    next();
+  } else {
+    var err = new Error( 'Please provide all fields (to, from, date) to the querystring.' );
+    err.status = 409;
+    next( err );
+  }
+ }, function( req, res ) {
+  thirdPartyHelpers.searchForFlight( req.query.to, req.query.from, req.query.date )
+  .then(function( matchingFlights ) {
+    res.send( matchingFlights );
+  })
+  .catch(function( error ) {
+    next( error );
+  });
+});
 
 module.exports = router;
