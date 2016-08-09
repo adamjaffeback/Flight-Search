68b32f929bd34c94f4452b58faed759138979f64
Adam Back <adb33@email.arizona.edu>
2016-08-08 21:07:47 -0700

    (init) Added start file.

---
+++ start.sh
@@ -0,0 +1,55 @@
+# borrowed from https://gist.github.com/JamieMason/4761049
+
+#!/bin/bash
+
+# Functions ==============================================
+
+# return 1 if global command line program installed, else 0
+# example
+# echo "node: $(program_is_installed node)"
+function program_is_installed {
+  # set to 1 initially
+  local return_=1
+  # set to 0 if not found
+  type $1 >/dev/null 2>&1 || { local return_=0; }
+  # return value
+  echo "$return_"
+}
+
+# display a message in red with a cross by it
+# example
+# echo echo_fail "No"
+function echo_fail {
+  # echo first argument in red
+  printf "\e[31m✘ ${1}"
+  # reset colours back to normal
+  echo "\033[0m"
+}
+
+# display a message in green with a tick by it
+# example
+# echo echo_fail "Yes"
+function echo_pass {
+  # echo first argument in green
+  printf "\e[32m✔ ${1}"
+  # reset colours back to normal
+  echo "\033[0m"
+}
+
+# echo pass or fail
+# example
+# echo echo_if 1 "Passed"
+# echo echo_if 0 "Failed"
+function echo_if {
+  if [ $1 == 1 ]; then
+    echo_pass $2
+  else
+    echo_fail $2
+  fi
+}
+
+echo "Thanks for reviewing my code challenge!"
+echo "Checking if necessary dev environment exists..."
+
+echo "node          $(echo_if $(program_is_installed node))"
+echo "npm          $(echo_if $(program_is_installed npm))"
