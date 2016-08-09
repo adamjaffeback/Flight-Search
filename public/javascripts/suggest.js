01f4c2027a962c60796008c83ab8b0e67eb3d833
Adam Back <adb33@email.arizona.edu>
2016-08-09 10:52:04 -0700

    (feat) Created dropdown suggestions based on city location.

--- public/javascripts/suggest.js
+++ public/javascripts/suggest.js
@@ -1,23 +1,35 @@
 global.jQuery = require( 'jquery' );
 
-jQuery(function() {
-  jQuery( '#from' ).autocomplete({
+var formatDropdownSuggestions = function( rawMatches ) {
+  rawMatches = JSON.parse( rawMatches );
+  var formattedSuggestions = [];
+
+  for ( var i = 0; i < rawMatches.length; i++ ) {
+    var match = rawMatches[ i ];
+    formattedSuggestions.push( match.cityName + ', ' + match.stateCode + ', ' + match.countryCode + ' (' + match.airportCode + ')' );
+  }
+
+  return formattedSuggestions;
+};
+
+var attachAutocomplete = function( targetEl ) {
+  jQuery( targetEl ).autocomplete({
     source: function( req, res ) {
       jQuery.ajax({
         url: 'airports?q=' + req.term,
         success: function( matches ) {
-          matches = JSON.parse( matches );
-          console.log( matches );
-          var formattedSuggestions = [];
-
-          for ( var i = 0; i < matches.length; i++ ) {
-            var match = matches[ i ];
-            formattedSuggestions.push( match.cityName + ', ' + match.stateCode + ', ' + match.countryCode + ' (' + match.airportCode + ')' );
-          }
-
-          res( formattedSuggestions );
+          res( formatDropdownSuggestions( matches ) );
         }
       });
+    },
+    select: function( event, ui ) {
+      event.preventDefault();
+      jQuery( targetEl ).val( ui.item.value.slice( -4, -1 ) );
     }
   });
+};
+
+jQuery(function() {
+  attachAutocomplete( '#from' );
+  attachAutocomplete( '#to' );
 });
